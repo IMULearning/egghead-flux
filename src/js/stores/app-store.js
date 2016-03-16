c8c35869d81bc16d5b7948e1023438d3bc4d73da
Weinan Qiu <davidiamyou@gmail.com>
2016-03-16 02:57:27 -0400

    move logic to CartAPI

--- src/js/stores/app-store.js
+++ src/js/stores/app-store.js
@@ -1,60 +1,10 @@
 import { dispatch, register } from '../dispatchers/app-dispatcher'
 import AppConstants from '../constants/app-constants'
 import { EventEmitter } from 'events'
+import CartAPI from '../api/CartAPI'
 
 const CHANGE_EVENT = 'change'
 
-var _catalog = [];
-
-for (let i = 1; i < 9; i++) {
-	_catalog.push({
-		'id': 'Widget' + i,
-		'title': 'Widget #' + i,
-		'summary': 'A great widget',
-		'description': 'Lorem ipsum dolor sit amet.',
-		'cost': i
-	})
-}
-
-var cartItems = [];
-
-function _removeItem(item) {
-	cartItems.splice(cartItems.findIndex( i => i === item ), 1)
-}
-
-function _findCartItem(item) {
-	return cartItems.find(cartItem => cartItem.id === item.id)
-}
-
-function _increaseItem(item) {
-	item.qty++
-}
-
-function _decreaseItem(item) {
-	item.qty--
-	if (item.qty === 0) {
-		_removeItem(item)
-	}
-}
-
-function _addItem(item) {
-	const cartItem = _findCartItem(item)
-	if (!cartItem) {
-		cartItems.push(Object.assign({qty: 1}, item))
-	} else {
-		_increaseItem(cartItem)
-	}
-}
-
-function _cartTotals(initialQty = 0, initialTotal = 0) {
-	const qty = cartItems.reduce(
-		(prev, current) => prev + current.qty, 
-		initialQty);
-	const total = cartItems.reduce(
-		(prev, current) => prev + current.qty * current.cost, 
-		initialTotal);
-	return {qty, total}
-}
 
 const AppStore = Object.assign(EventEmitter.prototype, {
 	emitChange() {
@@ -70,32 +20,30 @@ const AppStore = Object.assign(EventEmitter.prototype, {
 	},
 
 	getCart() {
-		return cartItems;
+		return CartAPI.cartItems;
 	},
 
 	getCatalog() {
-		return _catalog.map(item => {
-			return Object.assign({}, item, cartItems.find( cItem => cItem.id === item.id ))
-		})
+		return CartAPI.getCatalog();
 	},
 
 	getCartTotals() {
-		return _cartTotals()
+		return CartAPI.cartTotals()
 	},
 
 	dispatcherIndex: register(function(action){
 		switch(action.actionType) {
 			case AppConstants.ADD_ITEM:
-				_addItem(action.item);
+				CartAPI.addItem(action.item);
 				break;
 			case AppConstants.REMOVE_ITEM:
-				_removeItem(action.item);
+				CartAPI.removeItem(action.item);
 				break;
 			case AppConstants.INCREASE_ITEM:
-				_increaseItem(action.item);
+				CartAPI.increaseItem(action.item);
 				break;
 			case AppConstants.DECREASE_ITEM:
-				_decreaseItem(action.item);
+				CartAPI.decreaseItem(action.item);
 				break;
 		}
 
